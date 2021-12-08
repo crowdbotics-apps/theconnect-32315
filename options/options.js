e78733e6ca3442b6b884770458a6826cdad751cd
Crowdbotics <team@crowdbotics.com>
2021-12-08 21:13:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "theconnect_32315",
+  url: "https://theconnect_32315.botics.co",
+  api: "https://theconnect_32315.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
